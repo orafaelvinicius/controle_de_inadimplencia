7db148155312096659eeaabcf50d03cad423f3b5
orafaelvinicius <jumbeba3@gmail.com>
2022-03-23 19:49:27 -0300

    Inserindo calculadora de inadimplencia

--- inadimplentes/models.py
+++ inadimplentes/models.py
@@ -1,6 +1,6 @@
-from datetime import date
 from django.db import models
-
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
 
 kitnet = (
     ('Pequeno', 'Pequeno'),
@@ -21,9 +21,30 @@ class Inquilino(models.Model):
     status_de_pagamentos = models.CharField(max_length=15, choices=status_adimplencia, default='ADIMPLENTE')
     valor_do_aluguel = models.DecimalField(max_digits=13, decimal_places=2, blank=True, null=True)
     ultimo_pagamento = models.DateField(blank=True) 
-    meses_de_inadimplencia = models.CharField(max_length=255, default='0')
+    inadimplencia = models.CharField(max_length=255, default='0')
 
     def __str__(self):
         return self.nome
 
-   
+    @property
+    def calcula_inadimplencia(self):
+        ultimo_pagamento = self.ultimo_pagamento
+        data_de_hoje = datetime.now()
+
+        diff = relativedelta(data_de_hoje, ultimo_pagamento)
+
+        if diff.months >= 1 or diff.years > 0:
+            self.inadimplencia = diff.months
+
+            return self.inadimplencia
+        else:
+            self.inadimplencia = '0'
+            return self.inadimplencia
+            
+    def percentual_inadimplencia(self):
+        total_inquilinos = 100
+        total_inadimplentes = 30 
+
+        percentual_inadimplentes  = int((total_inadimplentes / total_inquilinos ) * 100)
+
+        print(f'Atualmente o percentual de inadimplentes é de {percentual_inadimplentes} %')
